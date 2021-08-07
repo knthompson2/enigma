e1866fd8f3a25274865cb3778e7cf98c73296361
Kelsey Thompson <kelsthompson2@gmail.com>
2021-08-07 08:05:10 -0400

    Write first part of encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,19 @@
 class Enigma
+  attr_reader :character_set
 
-def initialize
-end
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
 
-def todays_date
-  Time.now.strftime("%d%m%y")
-end
+  def todays_date
+    Time.now.strftime("%d%m%y")
+  end
 
+  def encrypt(string, key = shift.key_string, date = todays_date)
+    message = string.downcase
+    message_by_char = message.split("").map do |character|
+      character
+    end
 
+  end
 end
