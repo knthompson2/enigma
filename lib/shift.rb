56b9a3af3fe418d593fd53b61ea219526d264d47
Kelsey Thompson <kelsthompson2@gmail.com>
2021-08-06 20:08:35 -0400

    Write test and method for 5 digit key number

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,10 +3,17 @@ class Shift
   def initialize
   end
 
-  def offset(date)
+  def offset_number(date)
     date_squared = date.to_i ** 2
     date_squared.to_s[-4..-1]
   end
 
-  
+  def key_number
+    string = ''
+    string << "0"
+    4.times do
+      string << rand(9).to_s
+    end
+    string
+  end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe "Shift" do
 
   it "determines the offset" do
     shift = Shift.new
-    expect(shift.offset("080621")).to eq("5641")
+    expect(shift.offset_number("080621")).to eq("5641")
+  end
+
+  it "can generate a random 4 digit number and put a 0 at the beginning" do
+    shift = Shift.new
+    expect(shift.key_number.length).to eq(5)
+    expect(shift.key_number.class).to eq(String)
   end
 end
