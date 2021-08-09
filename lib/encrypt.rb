de2df14cb041609a4d3f2c1f89e05a293ef296a0
Kelsey Thompson <kelsthompson2@gmail.com>
2021-08-09 12:34:25 -0400

    Command line interface for encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,9 +1,11 @@
-
-require 'date'
 require './lib/enigma'
-require './lib/command_line'
 
-data = ARGV
-
-new_cli = CommandLine.new(data)
-puts new_cli.encrypt_message
+handle = File.open(ARGV[0], "r")
+message = handle.read
+enigma = Enigma.new
+encrypted = enigma.encrypt(message)
+writer = File.new(ARGV[1], "w")
+writer.write(encrypted[:encryption])
+handle.close
+writer.close
+puts "Created '#{ARGV[1]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
