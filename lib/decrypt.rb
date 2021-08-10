b2a9dcac1c772e61a543da7c548ebf9f74612176
Kelsey Thompson <kelsthompson2@gmail.com>
2021-08-09 20:39:07 -0400

    Complete decrypt runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,15 +1,17 @@
 require './lib/enigma'
-require './lib/key'
-require './lib/offset'
 
 handle = File.open(ARGV[0], "r")
 message = handle.read
 enigma = Enigma.new
 key = ARGV[2]
 date = ARGV[3]
-decrypted = enigma.decrypt(message, key, date)
-writer = File.new(ARGV[1], "w")
-writer.write(decrypted[:decryption])
-handle.close
-writer.close
-puts "Created '#{ARGV[1]}' with the key #{decrypted[:key]} and date #{decrypted[:date]}"
+if key && date
+  decrypted = enigma.decrypt(message, key, date)
+  writer = File.new(ARGV[1], "w")
+  writer.write(decrypted[:decryption])
+  handle.close
+  writer.close
+  puts "Created '#{ARGV[1]}' with the key #{decrypted[:key]} and date #{decrypted[:date]}"
+else
+  puts "Sorry, I need a key and date to decrypt this message."
+end
